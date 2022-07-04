2ebc8d6bce03b56d256b82b2f2901adeab72c03b
msousa <msousa.dev@outlook.com>
2022-07-03 22:15:26 -0300

    Contact

---
+++ database/migrations/2022_07_03_222926_create_site_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiteContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', '50');
+            $table->string('tel', '20');
+            $table->string('email', '100');
+            $table->integer('reason_contact');
+            $table->text('message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_contacts');
+    }
+}
