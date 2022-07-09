a50bb141ec97f6ceb2a120f1094eddccf1407c29
msousa <msousa.dev@outlook.com>
2022-07-09 18:26:04 -0300

    Migrations

--- app/Provider.php
+++ app/Provider.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Provider extends Model
 {
+    use SoftDeletes;
     //
     // protected $table = 'fornecedores';
     protected $fillable = ['name', 'site', 'uf', 'email'];
--- database/migrations/2022_07_09_211104_alter_providers_softdelete.php
+++ database/migrations/2022_07_09_211104_alter_providers_softdelete.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProvidersTable extends Migration
+class AlterProvidersSoftdelete extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateProvidersTable extends Migration
      */
     public function up()
     {
-        Schema::create('providers', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 50);
-            $table->timestamps();
-            // $table->softDeletes();
+        Schema::table('providers', function (Blueprint $table){
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +25,8 @@ class CreateProvidersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('providers');
+        Schema::table('providers', function (Blueprint $table){
+            $table->dropSoftDeletes();
+        });
     }
 }
